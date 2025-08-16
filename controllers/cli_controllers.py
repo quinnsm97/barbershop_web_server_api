2724d4c922a392cf5f8a3b63a35e509fd86e0454
quinnsm97 <199311766+quinnsm97@users.noreply.github.com>
2025-08-17 08:44:11 +1000

    feat(cli_controllers): Created cli commands to create drop and seed tables within psql database

---
+++ controllers/cli_controllers.py
@@ -0,0 +1,32 @@
+from flask import Blueprint
+from init import db
+from models.customer import Customer
+
+db_commands = Blueprint("db", __name__)
+
+@db_commands.cli.command("create")
+def create_table():
+    db.create_all()
+    print("Tables created.")
+
+@db_commands.cli.command("drop")
+def drop_tables():
+    db.drop_all()
+    print("Tables dropped")
+
+@db_commands.cli.command("seed")
+def seed_tables():
+    #Instance of the model
+    customers = [
+        Customer(
+            first_name="Alice",
+            last_name="Wonderland",
+            email="alice@gmail.com",
+            phone="0400000000"
+        ), Customer(
+            first_name="Bob",
+            last_name="Builder",
+            email="bob@gmail.com",
+            phone="0400000001"
+        )
+    ]
--- models/customer.py
+++ models/customer.py
@@ -6,8 +6,8 @@ class Customer(db.Model):
     customer_id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False unique=True)
-    phone = db.Column(db.String(50), nullable=False unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    phone = db.Column(db.String(50), nullable=False, unique=True)
 
 class CustomerSchema(SQLAlchemyAutoSchema):
     class Meta:
