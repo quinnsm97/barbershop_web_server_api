ca83f0fc380399095270cd344567c2e89abeea57
quinnsm97 <199311766+quinnsm97@users.noreply.github.com>
2025-08-17 15:00:24 +1000

    feat: Define a route to fetch customer by ID

--- controllers/customer_controller.py
+++ controllers/customer_controller.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from init import db
-from models.customer import Customer, customer_schemas
+from models.customer import Customer, customer_schemas, customer_schema
 
 customer_bp = Blueprint("customer", __name__, url_prefix="/customers")
 
@@ -20,6 +20,21 @@ def get_students():
         return {"message": "No customer records found."}, 404
     
 # GET /id
+@customer_bp.route("/<int:customer_id>")
+def get_a_customer(customer_id):
+    # Define a statement
+    stmt = db.select(Customer).where(Customer.customer_id == customer_id)
+    # Execute
+    customer = db.session.scalar(stmt)
+
+    if customer:
+        # Serialise
+        data = customer_schema.dump(customer)
+        # Return data
+        return jsonify(data)
+    else:
+        return {"message": f"Customer with id {customer_id} does not exist"}, 404
+
 # POST /
 # PUT/PATCH /id
 # DELETE /id
