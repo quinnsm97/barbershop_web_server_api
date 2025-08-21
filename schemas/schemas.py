6b9172ca8f39a9cd8512b654ac822e7974edd1b2
quinnsm97 <199311766+quinnsm97@users.noreply.github.com>
2025-08-22 06:47:26 +1000

    feat(schemas): seperated schemas into own file, created appointments schema)

---
+++ schemas/schemas.py
@@ -0,0 +1,40 @@
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow import fields
+
+from models.customer import Customer
+from models.staff import Staff
+from models.appointment import Appointment
+
+class CustomerSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Customer
+        load_instance = True
+
+# Single entry
+customer_schema = CustomerSchema()
+# Multiple entries
+customer_schemas = CustomerSchema(many=True)
+
+class StaffSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Staff
+        load_instance = True
+
+# Single entry
+staff_schema = StaffSchema()
+# Multiple entries
+staff_schemas = StaffSchema(many=True)
+
+class AppointmentSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Appointment
+        load_instance = True
+        include_fk = True
+        include_relationships = True
+    customer = fields.Nested("CustomerSchema", only=("id", "first_name", "last_name"))
+    staff = fields.Nested("StaffSchema", only=("id", "first_name", "last_name", "specialty"))
+
+# Single entry
+appointment_schema = AppointmentSchema()
+# Multiple entries
+appointment_schemas = AppointmentSchema(many=True) 
