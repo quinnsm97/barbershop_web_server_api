448595768172201ed86f959db5bf0ae3d79223cf
quinnsm97 <199311766+quinnsm97@users.noreply.github.com>
2025-08-25 06:55:38 +1000

    fix(main.py): reorganise imports to match convention

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import os
+
 from flask import Flask
+
 from controllers.cli_controller import db_commands
 from controllers.customer_controller import customer_bp
 from controllers.staff_controller import staff_bp
-
+from controllers.appointment_controller import appointment_bp
 from init import db
-import os
+
 
 def create_app():
     app = Flask(__name__)
@@ -18,5 +21,6 @@ def create_app():
     app.register_blueprint(db_commands)
     app.register_blueprint(customer_bp)
     app.register_blueprint(staff_bp)
+    app.register_blueprint(appointment_bp)
 
     return app
