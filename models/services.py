7e58473b452fc9d60f35a07e1f747536ffc05ee0
quinnsm97 <199311766+quinnsm97@users.noreply.github.com>
2025-08-25 19:16:12 +1000

    Delete models/services.py

--- models/services.py
+++
@@ -1,8 +0,0 @@
-from init import db
-
-class Service(db.Model):
-     __tablename__ = "services"
-     id = db.Column(db.Integer, primary_key=True)
-     name = db.Column(db.String(50), unique=True, nullable=False)
-     price = db.Column(db.Float, nullable=False)
-     duration_minutes = db.Column(db.Integer, default=30)
