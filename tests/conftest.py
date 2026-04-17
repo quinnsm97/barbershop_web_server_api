f17bb4b78ae44eacad7eb7ac8a10c96a6dc57918
quinnsm97 <199311766+quinnsm97@users.noreply.github.com>
2026-04-17 10:33:50 +1000

    test: add pytest setup with in-memory database

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from main import create_app
+from init import db
+
+@pytest.fixture
+def app():
+    app = create_app()
+    
+    app.config["TESTING"] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.drop_all()
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
